6a09bf1228a34e252e5b77d58e1d94537a0a440b
Nicolas Kasevich <n.kasevich@gmail.com>
2023-05-04 18:00:00 +0200

    Added google openid

--- app/endpoints/__init__.py
+++ app/endpoints/__init__.py
@@ -1,4 +1,5 @@
 from app.endpoints.healthcheck import health_checks
 from app.endpoints.home import router as home_router
+from app.endpoints.login import router as login_router
 
-__all__ = ["home_router", "health_checks"]
+__all__ = ["home_router", "health_checks", "login_router"]
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import os
 from fastapi import FastAPI
 from fastapi_health import health
 
-from app.endpoints import health_checks, home_router
+from app.endpoints import health_checks, home_router, login_router
+
+from starlette.middleware.sessions import SessionMiddleware
+
+from dotenv import load_dotenv
 
 
 def create_app():
@@ -11,8 +16,13 @@ def create_app():
 
     # Add routers
     app.include_router(home_router, tags=["Home"])
+    app.include_router(login_router, tags=["Login"])
+
+    # Middlewares
+    app.add_middleware(SessionMiddleware, secret_key=os.environ.get("APP_KEY"))
 
     return app
 
 
+load_dotenv()
 app = create_app()
