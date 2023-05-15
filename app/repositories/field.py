1ff4d76792025db7f6750a9ade627fe9cd169097
Nicolas Kasevich <n.kasevich@gmail.com>
2023-05-15 18:05:53 +0200

    Get field by id

--- app/repositories/field.py
+++ app/repositories/field.py
@@ -9,6 +9,10 @@ class FieldRepository(BaseRepository):
         stmt = select(Field).where(Field.deleted_at.is_(None))
         return self.session.execute(stmt).scalars().all()
 
+    def get(self, id: int) -> Field | None:
+        stmt = select(Field).where(Field.id == id)
+        return self.session.execute(stmt).scalars().first()
+
     def get_by_tag(self, tag_id: int) -> list[Field]:
         stmt = select(Field).where(Field.tags.any(Tag.id == tag_id))
         return self.session.execute(stmt).scalars().all()
